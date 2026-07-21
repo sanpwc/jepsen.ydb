5349d6302f9c83ca9ed796901b2a16d167080656
Alexander Lapin <alapinpwc@ydb.tech>
2026-07-21 14:59:36 +0300

    JEPSEN-12 Fix test.

--- test/jepsen/ydb/ydb_test.clj
+++ test/jepsen/ydb/ydb_test.clj
@@ -3,13 +3,13 @@
             [jepsen.ydb :as ydb]))
 
 (deftest validate-opts-test
-  (testing "Valid: serializable without opindex"
-    (is (= {:model :serializable :with-opindex false}
-           (ydb/validate-opts {:model :serializable :with-opindex false}))))
+  (testing "Valid: ydb-serializable without opindex"
+    (is (= {:model :ydb-serializable :with-opindex false}
+           (ydb/validate-opts {:model :ydb-serializable :with-opindex false}))))
 
-  (testing "Valid: serializable with opindex"
-    (is (= {:model :serializable :with-opindex true}
-           (ydb/validate-opts {:model :serializable :with-opindex true}))))
+  (testing "Valid: ydb-serializable with opindex"
+    (is (= {:model :ydb-serializable :with-opindex true}
+           (ydb/validate-opts {:model :ydb-serializable :with-opindex true}))))
 
   (testing "Valid: snapshot-isolation without opindex"
     (is (= {:model :snapshot-isolation :with-opindex false}
@@ -46,7 +46,7 @@
 
   (testing "Invalid: check error message"
     (is (thrown-with-msg? IllegalArgumentException
-                          #"--with-opindex is not compatible with --model snapshot-isolation"
+                          #"with-opindex can be used with --model ydb-serializable only"
                           (ydb/validate-opts {:model :snapshot-isolation
                                               :with-opindex true})))))
 
